71631cec659ad1724dcf497a7bf0c0f16ab6c841
Antonio <ntoniohu@gmail.com>
2020-04-22 23:34:50 +0200

    Fix typo

--- src/ethoscope/core/qc.py
+++ src/ethoscope/core/qc.py
@@ -8,8 +8,8 @@ class QualityControl:
         self.result_writer = result_writer
 
         result_writer._create_table(
-            "QC", "t INT, mean FLOAT, min FLOAT, max FLOAT,
-            )
+            "QC", "t INT, mean FLOAT, min FLOAT, max FLOAT
+        )
 
     @staticmethod
     def image_stat(frame):
@@ -23,7 +23,7 @@ class QualityControl:
     def write(self, t, qc):
         #(t, mean_red, mean_green, mean_blue, min_red, max_red, min_green, max_green, min_blue, max_blue) \
         tp = (t, qc['stat'].mean[0], qc['stat'].extrema[0][0], qc['stat'].extrema[0][1])
-        
+
         command = f"INSERT INTO QC VALUES {str(tp)}"
 
         if "QC" not in self.result_writer._insert_dict or self.result_writer._insert_dict == "":
