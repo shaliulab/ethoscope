1d59e9460cc068fce1bf35b95ac59d571114bb8a
antortjim <ntoniohu@gmail.com>
2020-04-28 21:13:54 +0100

    local variable roi_colour referenced before assignment

--- src/ethoscope/drawers/drawers.py
+++ src/ethoscope/drawers/drawers.py
@@ -126,6 +126,7 @@ class DefaultDrawer(BaseDrawer):
         if img is None:
             return
 
+        roi_colour = (0, 255,0)
         cv2.drawContours(img, [self.arena], -1, roi_colour, 1, LINE_AA)
         for track_u in tracking_units:
 
@@ -134,7 +135,6 @@ class DefaultDrawer(BaseDrawer):
 
             cv2.putText(img, str(track_u.roi.idx), (int(x),int(y)), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,255,0))
             black_colour = (0, 0,0)
-            roi_colour = (0, 255,0)
             cv2.drawContours(img,[track_u.roi.polygon],-1, black_colour, 3, LINE_AA)
             cv2.drawContours(img,[track_u.roi.polygon],-1, roi_colour, 1, LINE_AA)
 
