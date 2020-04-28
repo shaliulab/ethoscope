4c4dd2e137496b519184667ed83421b426fb6012
antortjim <ntoniohu@gmail.com>
2020-04-28 20:14:09 +0100

    Fix typo

--- src/ethoscope/roi_builders/roi_builders.py
+++ src/ethoscope/roi_builders/roi_builders.py
@@ -43,8 +43,9 @@ class BaseROIBuilder(DescribedObject):
                     break
 
             accum = np.median(np.array(accum),0).astype(np.uint8)
+            import cv2
             cv2.imwrite('/root/actual_image.png', accum)
-            
+
         try:
             if self.__class__.__name__ == "FSLTargetROIBuilder":
                 img, M, rois = self._rois_from_img(accum)
