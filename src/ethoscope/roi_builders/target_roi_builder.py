43d168dd721ec4573a391a93c82d208646eb690e
antortjim <ntoniohu@gmail.com>
2020-05-07 00:52:33 +0200

    Fixes

--- src/ethoscope/roi_builders/target_roi_builder.py
+++ src/ethoscope/roi_builders/target_roi_builder.py
@@ -89,7 +89,8 @@ class TargetGridROIBuilder(BaseROIBuilder):
         #     self._right_margin = self._left_margin
         # if self._bottom_margin is None:
         #     self._bottom_margin = self._top_margin
-        _target_coord_file = kwargs.pop('target_coordinates_file')
+        if 'target_coordinates_file' in kwargs.keys():
+            self._target_coord_file = kwargs.pop('target_coordinates_file')
 
         super(TargetGridROIBuilder,self).__init__(*args, **kwargs)
 
