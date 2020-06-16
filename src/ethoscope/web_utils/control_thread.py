9d3a2572e39189862c053511bbbf47a8b5fb2824
antortjim <ntoniohu@gmail.com>
2020-06-16 18:23:53 +0000

    Let user choose camera in html modal

--- src/ethoscope/web_utils/control_thread.py
+++ src/ethoscope/web_utils/control_thread.py
@@ -109,7 +109,7 @@ class ControlThread(Thread):
     #some classes do not need to be offered as choices to the user in normal conditions
     #these are shown only if the machine is not a PI
     _is_a_rPi = isMachinePI() and hasPiCamera() and not isExperimental()
-    _hidden_options = {'camera', 'result_writer'}
+    _hidden_options = {'result_writer'}
 
     for k in _option_dict:
         _option_dict[k]["class"] =_option_dict[k]["possible_classes"][0]
