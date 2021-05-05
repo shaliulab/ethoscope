78519c6f2244bfde6b8739e8536d6e03c5c3b647
antortjim <ntoniohu@gmail.com>
2021-05-05 21:40:23 +0000

    Update sd testing scripts

--- scripts/tools/sd_server.py
+++ scripts/tools/sd_server.py
@@ -1,70 +1,87 @@
 import logging
 import time
+import threading
+
 import bottle
+
 from ethoscope.hardware.interfaces.optomotor import SleepDepriver
 
 
-api = bottle.Bottle()
-sd = SleepDepriver(do_warm_up=False)
-PORT=9001
-DEBUG=True
+class SDServer(threading.Thread):
+
+    def __init__(self, *args, port=9001, debug=True, **kwargs):
+
+        self._port = port
+        self._debug = debug
+        self.sd = SleepDepriver(do_warm_up=False)
+        super().__init__(*args, **kwargs)
 
 
-def run(api):
-
-    class CherootServer(bottle.ServerAdapter):
-        def run(self, handler): # pragma: no cover
-            from cheroot import wsgi
-            from cheroot.ssl import builtin
-            self.options['bind_addr'] = (self.host, self.port)
-            self.options['wsgi_app'] = handler
-            certfile = self.options.pop('certfile', None)
-            keyfile = self.options.pop('keyfile', None)
-            chainfile = self.options.pop('chainfile', None)
-            server = wsgi.Server(**self.options)
-            if certfile and keyfile:
-                server.ssl_adapter = builtin.BuiltinSSLAdapter(
-                        certfile, keyfile, chainfile)
-            try:
-                server.start()
-            finally:
-                server.stop()
-
-    ####### THIS IS A BIG MESS AND NEEDS TO BE FIXED. To be remove when bottle changes to version 0.13
+    def run(self):
     
-    SERVER = "cheroot"
-    try:
-        #This checks if the patch has to be applied or not. We check if bottle has declared cherootserver
-        #we assume that we are using cherrypy > 9
-        from bottle import CherootServer
-    except:
-        #Trick bottle to think that cheroot is actulay cherrypy server, modifies the server_names allowed in bottle
-        #so we use cheroot in background.
-        SERVER = "cherrypy"
-        bottle.server_names["cherrypy"] = CherootServer(host='0.0.0.0', port=PORT)
-        logging.warning("Cherrypy version is bigger than 9, change to cheroot server")
-        pass
-    #########
+        class CherootServer(bottle.ServerAdapter):
+            def run(self, handler): # pragma: no cover
+                from cheroot import wsgi
+                from cheroot.ssl import builtin
+                self.options['bind_addr'] = (self.host, self.port)
+                self.options['wsgi_app'] = handler
+                certfile = self.options.pop('certfile', None)
+                keyfile = self.options.pop('keyfile', None)
+                chainfile = self.options.pop('chainfile', None)
+                server = wsgi.Server(**self.options)
+                if certfile and keyfile:
+                    server.ssl_adapter = builtin.BuiltinSSLAdapter(
+                            certfile, keyfile, chainfile)
+                try:
+                    server.start()
+                finally:
+                    server.stop()
     
-    bottle.run(api, host='0.0.0.0', port=PORT, debug=DEBUG, server=SERVER)
+        ####### THIS IS A BIG MESS AND NEEDS TO BE FIXED. To be remove when bottle changes to version 0.13
+        
+        SERVER = "cheroot"
+        try:
+            #This checks if the patch has to be applied or not. We check if bottle has declared cherootserver
+            #we assume that we are using cherrypy > 9
+            from bottle import CherootServer
+        except:
+            #Trick bottle to think that cheroot is actulay cherrypy server, modifies the server_names allowed in bottle
+            #so we use cheroot in background.
+            SERVER = "cherrypy"
+            bottle.server_names["cherrypy"] = CherootServer(host='0.0.0.0', port=self._port)
+            logging.warning("Cherrypy version is bigger than 9, change to cheroot server")
+            pass
+        #########
+        
+        app = self.routeapp()
+        bottle.run(app, host='0.0.0.0', port=self._port, debug=self._debug, server=SERVER)
+    
+    #@api.post("/activate")
+    def activate(self):
+        data = bottle.request.json
+        duration  = int(data["duration"])
+    
+        if type(data["channels"]) is int:
+            channels = [data["channels"]]
+        elif type(data["channels"]) is list:
+            channels = data["channels"]
+        else:
+            logging.warning("Pass int or list in channel field")
+            return 1 
+    
+        for channel in channels:
+            self.sd.activate(channel=channel, duration=duration)
+        return 0
 
-@api.post("/activate")
-def activate():
-    data = bottle.request.json
-    duration  = int(data["duration"])
-
-    if type(data["channels"]) is int:
-        channels = [data["channels"]]
-    elif type(data["channels"]) is list:
-        channels = data["channels"]
-    else:
-        logging.warning("Pass int or list in channel field")
-        return 1 
-
-    for channel in channels:
-        sd.activate(channel=channel, duration=duration)
-        time.sleep(2)
-    return 0
+    activate.post = "/activate"
 
 
-run(api)
+    def routeapp(app):
+
+        for kw in dir(app):
+            attr = getattr(app, kw)
+            if hasattr(attr, 'route'):
+                bottle.route(attr.route)(attr)
+
+            if hasattr(attr, 'post'):
+                bottle.post(attr.post)(attr)
--- scripts/tools/test_sd.py
+++ scripts/tools/test_sd.py
@@ -2,21 +2,94 @@ import argparse
 import logging
 import subprocess 
 import os
-
-ap = argparse.ArgumentParser()
-ap.add_argument("-r", "--region-id", nargs="+", required=True, type=int)
-ap.add_argument("--duration", type=int, default=1000)
-ap.add_argument("--port", type=int, default=9001)
-args = ap.parse_args()
+import warnings
+from sd_server import SDServer
 
 
-roi_to_channel = {1:1, 3:3, 5:5, 7:7, 9:9, 12:11, 14:13, 16:15, 18:17, 20:19}
+class SDClient:
 
-channels = [str(roi_to_channel[roi]) for roi in args.region_id]
-channels_str = ", ".join(channels)
-channels_str = "[" + channels_str + "]"
-print(channels_str)
-print(channels)
-cmd = "/usr/bin/curl -X POST -H \"Content-Type: application/json\" -d '{\"channels\": %s, \"duration\": %d}' localhost:%d/activate" % (channels_str, args.duration, args.port)
-os.system(cmd)
-print(cmd)
+    def __init__(self, port):
+        self._port = port
+        self._roi_to_channel = {1:1, 3:3, 5:5, 7:7, 9:9, 12:11, 14:13, 16:15, 18:17, 20:19}
+
+    def get_channels(self, region_ids):
+        channels = []
+        for roi in region_ids:
+            if roi in self._roi_to_channel:
+                channels.append(str(self._roi_to_channel[roi]))
+
+        channels_str = ", ".join(channels)
+        channels_str = "[" + channels_str + "]"
+        return channels_str
+
+    def activate(self, region_ids, duration=1000):
+
+        binary = "/usr/bin/curl"
+        method = "-X POST"
+        header = "-H \"Content-Type: application/json\""
+        
+        channels = self.get_channels(region_ids)
+        dictionary = "-d '{\"channels\": %s, \"duration\": %d}'" % (channels, duration)
+
+        dest = f"localhost:{self._port}/activate"
+
+        cmd = f"{binary} {method} {header} {dictionary} {dest}"
+        os.system(cmd)
+
+
+    @staticmethod
+    def format_region_ids(region_ids):
+        region_ids = region_ids.split(" ")
+        region_ids_int = []
+        for r in region_ids:
+            if r == "":
+                pass
+            else:
+                region_ids_int.append(int(r))
+
+        return region_ids_int
+
+
+    def run(self):
+
+        while True:
+
+            try:
+                region_ids = input("Please enter region ids. Example: 1 3 5 20.\n")
+                if region_ids == "":
+                    warnings.warn("Please enter a valid region_id")
+                    continue
+
+                region_ids = self.format_region_ids(region_ids)
+                
+                duration  = input("Please enter stimulus duration in ms [1000]\n")
+                if duration == "":
+                    duration = 1000
+
+                self.activate(region_ids, duration)
+
+            except KeyboardInterrupt:
+                break
+
+        return 0
+
+    def start(self):
+        return self.run()
+
+
+
+if __name__ == "__main__":
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument("--port", type=int, default=9001)
+    args = ap.parse_args()
+
+
+    sd_server = SDServer(port=args.port, debug=True)
+    sd_server.setDaemon(True)
+    sd_server.start()
+
+    sd_client = SDClient(port=args.port)
+    sd_client.start()
+
+#cmd = "/usr/bin/curl -X POST -H \"Content-Type: application/json\" -d '{\"channels\": %s, \"duration\": %d}' localhost:%d/activate" % (channels_str, args.duration, args.port)
