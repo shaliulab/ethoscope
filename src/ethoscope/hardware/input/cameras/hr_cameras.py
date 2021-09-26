282eda9fb25121d025ce55cc121d4d310d7678f5
Antonio Ortega <ntoniohu@gmail.com>
2021-09-26 22:45:37 +0200

    useColor=False

--- src/ethoscope/hardware/input/cameras/hr_cameras.py
+++ src/ethoscope/hardware/input/cameras/hr_cameras.py
@@ -28,6 +28,7 @@ class HRPiCameraAsync(OurPiCameraAsync):
             videos_dir = f"/ethoscope_data/results/{machine_id}/{machine_name}"
             kwargs = {
                   "mode": 'w',
+                  "isColor": False,
                   "framerate": framerate,
                   "basedir": f"{videos_dir}/{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}",
                   "imgshape": resolution[::-1], # reverse order so it becomes nrows x ncols i.e. height x width
