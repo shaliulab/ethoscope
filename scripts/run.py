dfe98d8205023fdf115870c31dc64d61549104b3
Antonio Ortega <ntoniohu@gmail.com>
2021-09-26 19:06:09 +0200

    Update run script

--- scripts/run.py
+++ scripts/run.py
@@ -3,7 +3,7 @@ import logging
 
 from ethoscope.roi_builders.automatic_roi_builder import AutomaticROIBuilder
 from ethoscope.core.monitor import Monitor
-from ethoscope.hardware.input.cameras import MovieVirtualCamera
+from ethoscope.hardware.input.cameras import MovieVirtualCamera, ImgStoreCamera
 from ethoscope.trackers.rich_adaptive_bg_tracker import RichAdaptiveBGModel
 from ethoscope.utils.io import SQLiteResultWriter
 from ethoscope.core.qc import QualityControl
@@ -11,12 +11,12 @@ from ethoscope.drawers.drawers import DefaultDrawer
 # from ethoscope.stimulators.stimulators import DefaultStimulator
 from ethoscope.web_utils.helpers import get_machine_id, get_machine_name
 
-roi_builder = AutomaticROIBuilder()
-camera = MovieVirtualCamera("./data/video.avi")
+roi_builder = AutomaticROIBuilder(args=(), kwargs={"top_left": "(760, 180)", "roi_width": 3130, "roi_height": 231, "roi_offset": 340, "nrois": 9})
+camera = ImgStoreCamera("./data/2021-09-26_15-07-37/")
 rois = roi_builder.build(camera)
 
 # just analyze the 4th fly
-rois = [rois[3]]
+#rois = [rois[3]]
 
 sensor = None
 stimulators = None
@@ -32,7 +32,9 @@ metadata = {
     "selected_options": str({}),
 }
 
-db_credentials = {"name": "result.db", "user": "ethoscope", "password": "ethoscope"}
+
+output = "/ethoscope_data/results/5ce88718c3244cc88a1fa59d19099951/SUPERETHOSCOPE_001/2021-09-26_15-45-00/2021-09-26-15-45-00_5ce88718c3244cc88a1fa59d19099951.db"
+db_credentials = {"name": output, "user": "ethoscope", "password": "ethoscope"}
 
 logging.warning("Detected camera start time is %d", camera.start_time)
 # hardware_interface is a running thread
