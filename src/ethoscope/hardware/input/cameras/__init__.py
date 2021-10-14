4ccd172c06fe3989dacd87f25ae7469ac3e8c734
Antonio Ortega <ntoniohu@gmail.com>
2021-10-14 22:29:17 +0200

    Fix import

--- src/ethoscope/hardware/input/cameras/__init__.py
+++ src/ethoscope/hardware/input/cameras/__init__.py
@@ -1,5 +1,5 @@
-from .cameras import OurPiCameraAsync, MovieVirtualCamera, DummyPiCameraAsync, V4L2Camera, FSLVirtualCamera, JetsonNanoCamera
-from .hr_cameras import HRPiCameraAsync
+from .cameras import OurPiCameraAsync, MovieVirtualCamera, DummyPiCameraAsync, V4L2Camera, FSLVirtualCamera
+from .hr_cameras import HRPiCameraAsync, JetsonNanoCamera
 from .dual_cameras import FSLPiCameraAsync
 from .imgstore_camera import ImgStoreCamera
 from .camera_settings import *
