63b87d7a506bbef0290670da41c5c62c0e5ce15f
antortjim <ntoniohu@gmail.com>
2025-11-17 18:06:46 +0000

    Fix super calls

--- src/ethoscope/hardware/interfaces/interfaces.py
+++ src/ethoscope/hardware/interfaces/interfaces.py
@@ -79,7 +79,7 @@ class DynamicPWMHardwareconnection(HardwareConnection):
 
 
     def __init__(self, *args, **kwargs):
-        super(HardwareConnection).__init__(*args, **kwargs)
+        super(DynamicPWMHardwareconnection).__init__(*args, **kwargs)
         self.last_pwm_change=None
 
     def decide_pwm_change(self):
