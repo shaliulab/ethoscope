7d6bd213ad487b1bcf85215b9d6e02c101d0f108
ETedre <erik.tedre@gmail.com>
2026-01-28 18:09:33 +0100

    Add import numpy statement

--- src/ethoscope/stimulators/pwm_stimulators.py
+++ src/ethoscope/stimulators/pwm_stimulators.py
@@ -6,7 +6,7 @@ from ethoscope.hardware.interfaces.optomotor import OptoMotor, SleepDepriver, PW
 # from ethoscope.hardware.interfaces.interfaces import HardwareConnection, DynamicPWMHardwareConnection
 
 from ethoscope.stimulators.sleep_depriver_stimulators import RobustSleepDepriver
-
+import numpy as np
 import random
 import time
 import logging
